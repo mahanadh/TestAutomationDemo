caaf6e10058e0b555623ed14d458f441bc6afd2f
Mahan Adhikari <mahan2054@gmail.com>
2023-06-05 19:38:40 +0545

    bump

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+module.exports = {
+  // The rest of the Cypress config options go here...
+  projectId: "jwvuop",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+};
